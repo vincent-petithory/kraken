4b3547a2e85f4ab6bc5d96fc291210190cd95a9f
Vincent Petithory <vincent.petithory@gmail.com>
2014-08-16 22:48:35 +0200

    Expand API to manage server dir aliases

--- krakend/admin.go
+++ krakend/admin.go
@@ -19,8 +19,10 @@ type serverPoolAdminHandler struct {
 }
 
 const (
-	LocationServers     = "servers"
-	LocationServersPort = "servers.port"
+	LocationServers                = "servers"
+	LocationServersSelf            = "servers.self"
+	LocationServersSelfAliases     = "servers.self.aliases"
+	LocationServersSelfAliasesSelf = "servers.self.aliases.self"
 )
 
 type adminApiErrorType string
@@ -56,7 +58,16 @@ func NewServerPoolAdminHandler(serverPool *serverPool) *serverPoolAdminHandler {
 		"GET":    http.HandlerFunc(spah.getServer),
 		"PUT":    http.HandlerFunc(spah.createServer),
 		"DELETE": http.HandlerFunc(spah.removeServer),
-	}).Name(LocationServersPort)
+	}).Name(LocationServersSelf)
+	apiRouter.Handle("/servers/{port:[0-9]{1,5}}/aliases", handlers.MethodHandler{
+		"GET":    http.HandlerFunc(spah.getServerAliases),
+		"DELETE": http.HandlerFunc(spah.removeServerAliases),
+	}).Name(LocationServersSelfAliases)
+	apiRouter.Handle("/servers/{port:[0-9]{1,5}}/aliases/{alias}", handlers.MethodHandler{
+		"GET":    http.HandlerFunc(spah.getServerAlias),
+		"PUT":    http.HandlerFunc(spah.createServerAlias),
+		"DELETE": http.HandlerFunc(spah.removeServerAlias),
+	}).Name(LocationServersSelfAliasesSelf)
 	spah.h = r
 	spah.router = r
 	return &spah
@@ -97,6 +108,21 @@ func dirServerDataFromDirServer(ds *dirServer) dirServerData {
 	}
 }
 
+func (spah *serverPoolAdminHandler) serverOr404(w http.ResponseWriter, r *http.Request) *dirServer {
+	sport := mux.Vars(r)["port"]
+	port, err := strconv.Atoi(sport)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return nil
+	}
+	srv := spah.serverPool.Get(uint16(port))
+	if srv == nil {
+		http.Error(w, fmt.Sprintf("server %d not found", port), http.StatusNotFound)
+		return nil
+	}
+	return srv
+}
+
 func (spah *serverPoolAdminHandler) getServers(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	srvsData := make([]dirServerData, 0, len(spah.serverPool.Srvs))
@@ -109,18 +135,10 @@ func (spah *serverPoolAdminHandler) getServers(w http.ResponseWriter, r *http.Re
 }
 
 func (spah *serverPoolAdminHandler) getServer(w http.ResponseWriter, r *http.Request) {
-	sport := mux.Vars(r)["port"]
-	port, err := strconv.Atoi(sport)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	srv := spah.serverPool.Get(uint16(port))
+	srv := spah.serverOr404(w, r)
 	if srv == nil {
-		http.Error(w, fmt.Sprintf("server %d not found", port), http.StatusNotFound)
 		return
 	}
-
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(dirServerDataFromDirServer(srv)); err != nil {
 		log.Print(err)
@@ -147,7 +165,7 @@ func (spah *serverPoolAdminHandler) createServerWithRandomPort(w http.ResponseWr
 	}
 	// Wait for the server to be started
 	<-srv.started
-	spah.writeLocation(w, LocationServersPort, "port", strconv.Itoa(int(srv.Port)))
+	spah.writeLocation(w, LocationServersSelf, "port", strconv.Itoa(int(srv.Port)))
 	w.WriteHeader(http.StatusCreated)
 	if err := json.NewEncoder(w).Encode(dirServerDataFromDirServer(srv)); err != nil {
 		log.Print(err)
@@ -208,24 +226,97 @@ func (spah *serverPoolAdminHandler) removeServers(w http.ResponseWriter, r *http
 }
 
 func (spah *serverPoolAdminHandler) removeServer(w http.ResponseWriter, r *http.Request) {
-	sport := mux.Vars(r)["port"]
-	port, err := strconv.Atoi(sport)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	srv := spah.serverPool.Get(uint16(port))
+	srv := spah.serverOr404(w, r)
 	if srv == nil {
-		http.Error(w, fmt.Sprintf("server %d not found", port), http.StatusNotFound)
 		return
 	}
-
 	if ok, err := spah.serverPool.Remove(srv.Port); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	} else if !ok {
 		err := fmt.Errorf("error shutting down server on port %d", srv.Port)
 		log.Print(err)
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (spah *serverPoolAdminHandler) getServerAliases(w http.ResponseWriter, r *http.Request) {
+	srv := spah.serverOr404(w, r)
+	if srv == nil {
+		return
+	}
+	aliases := srv.DirAliases.List()
+	aliasesMap := make(map[string]string, len(aliases))
+
+	for _, alias := range aliases {
+		aliasesMap[alias] = srv.DirAliases.Get(alias)
+	}
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(aliasesMap); err != nil {
+		log.Print(err)
+	}
+}
+
+func (spah *serverPoolAdminHandler) removeServerAliases(w http.ResponseWriter, r *http.Request) {
+	srv := spah.serverOr404(w, r)
+	if srv == nil {
+		return
+	}
+	aliases := srv.DirAliases.List()
+	for _, alias := range aliases {
+		srv.DirAliases.Delete(alias)
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (spah *serverPoolAdminHandler) getServerAlias(w http.ResponseWriter, r *http.Request) {
+	srv := spah.serverOr404(w, r)
+	if srv == nil {
+		return
+	}
+	alias := mux.Vars(r)["alias"]
+	aliasPath := srv.DirAliases.Get(alias)
+	if aliasPath == "" {
+		http.Error(w, fmt.Sprintf("server %d has no alias %q", srv.Port, alias), http.StatusNotFound)
+		return
+	}
+	aliasesMap := map[string]string{alias: aliasPath}
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(aliasesMap); err != nil {
+		log.Print(err)
+	}
+}
+
+type createServerAliasRequest struct {
+	Path string `json:"path"`
+}
+
+func (spah *serverPoolAdminHandler) createServerAlias(w http.ResponseWriter, r *http.Request) {
+	srv := spah.serverOr404(w, r)
+	if srv == nil {
+		return
+	}
+	alias := mux.Vars(r)["alias"]
+	defer r.Body.Close()
+	var req createServerAliasRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	_ = srv.DirAliases.Put(alias, req.Path)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (spah *serverPoolAdminHandler) removeServerAlias(w http.ResponseWriter, r *http.Request) {
+	srv := spah.serverOr404(w, r)
+	if srv == nil {
+		return
+	}
+	alias := mux.Vars(r)["alias"]
+	ok := srv.DirAliases.Delete(alias)
+	if !ok {
+		http.Error(w, fmt.Sprintf("server %d has no alias %q", srv.Port, alias), http.StatusNotFound)
+		return
 	}
 	w.WriteHeader(http.StatusOK)
 }
